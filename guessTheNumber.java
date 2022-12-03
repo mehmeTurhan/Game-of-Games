e07d87ac8b7a1d5e452f28207a25bcf301c3683a
stowel2 <93685641+stowel2@users.noreply.github.com>
2022-12-03 16:37:41 -0500

    Add files via upload

---
+++ guessTheNumber.java
@@ -0,0 +1,180 @@
+/*
+ * header
+ */
+
+import java.util.*;
+
+public class guessTheNumber {
+
+    // declare variables
+    private int rangeLow;
+    private int rangeHigh;
+    private int compGuess;
+    private int playerGuess;
+    private int numGuesses;
+    private int numGuessesCounter = 0;
+    private final int ONE = 1;
+    private final int TWO = 2;
+    Scanner sc = new Scanner(System.in);
+
+    private int getRandomInt(int min, int max) {
+
+		Random r = new Random();
+		return r.nextInt((max - min) + ONE) + min;
+	}
+
+    // start display method
+    public void startDisplay() {
+
+        System.out.println(
+                "The players will specify the range of numbers and the number of guesses, however the number of guesses cannot be more than half the number of values in the range. So for instance, if the range is from 1-10, the user can make at most 5 guesses.");
+                System.out.println("\n"); // formatting output
+
+    }
+
+    // getRange method:
+    public void getRange() {
+
+        System.out.println("Select a range of numbers (input 2 integers)");
+        rangeLow = sc.nextInt();
+        rangeHigh = sc.nextInt();
+        System.out.println("\n"); // formatting output
+
+
+        /*
+         * if (rangeLow != integer)
+         *  ask!
+         */
+
+        /*          TEST MODE
+        System.out.println("\n"); // formatting output
+        System.out.println("Range Low:"); // formatting output
+        System.out.println(rangeLow); // formatting output
+        System.out.println("Range High: "); // formatting output
+        System.out.println(rangeHigh); // formatting output
+        */
+
+    }
+
+    // getNumGuesses method
+    public void getNumGuesses() {
+
+        final int MAX = (((rangeHigh - rangeLow) / TWO) + ONE);
+
+        System.out.println("Enter Number of Guesses"); // not specified in use cases BUT alluded to in description
+        numGuesses = sc.nextInt();
+        System.out.println("\n"); // formatting output
+
+        while (numGuesses > MAX) {
+
+            System.out.println("Number of Guesses too large. Must be less than or equal to half of the range.");
+            System.out.println("Enter Number of Guesses");
+            numGuesses = sc.nextInt();
+            System.out.println("\n"); // formatting output
+
+        }
+
+        /*      TEST MODE
+        System.out.println("Number of Guesses");
+        System.out.print(numGuesses);
+        System.out.println("\n"); // formatting output
+        */
+    }
+
+    /*
+     * getGuesses function is tentatively set as computer always chooses, user always guesses
+     */
+    public void getGuesses() {
+
+        // COMPUTER CHOOSES
+        compGuess = getRandomInt(rangeLow, rangeHigh);
+
+        // USER CHOOSES
+        System.out.println("Guess num:");
+        playerGuess = sc.nextInt();
+        System.out.println("\n"); // formatting output
+
+        while ((playerGuess > rangeHigh) || (playerGuess < rangeLow)){
+
+            System.out.println("please input a number within the range");
+            playerGuess = sc.nextInt();
+            System.out.println("\n"); // formatting output
+
+        }
+
+        numGuessesCounter += ONE; // increment by 1
+
+        // call checkGuesses
+        checkGuesses();
+
+        /*           TEST MODE
+        System.out.println("user guess:"); // formatting output
+        System.out.println(guessNum); // formatting output
+        System.out.println("comp guess:"); // formatting output
+        System.out.println(actualNum); // formatting output
+        */
+
+
+
+
+        /*
+         * 
+         * 
+         * 
+         * BE SURE TO REMOVE ON SUBMISSION TO OTHER TEAM
+         * 
+         * added bug to this section:
+         * 
+         * after the checkGuesses() method is called, it re-calls the getGuesses() method and tries again.
+         * 
+         * BUT THE getGuesses() METHOD ALSO CHOOSES ANOTHER NUMBER FOR COMPUTER UPON EACH INCORRECT ANSWER
+         * 
+         * 
+         * 
+         */
+
+    }
+
+    public void checkGuesses() {
+
+        if (compGuess == playerGuess){
+
+            System.out.println("Guess Correct! Player wins!");
+            // break to end, update scoreboard in main
+
+        }
+        else {  // implies guess was incorrect
+    
+            System.out.println("Incorrect Guess.");
+            System.out.println("\n"); // formatting output
+
+            // check if max guesses is NOT yet reached
+            if (numGuessesCounter < numGuesses){
+
+                System.out.println("You have " + (numGuesses - numGuessesCounter) + " guesses to go.");
+                System.out.println("\n"); // formatting output
+                getGuesses();
+
+            }
+            else {  // implies max guesses has been reached
+
+                System.out.println("You lose!");
+                // break to end, update scoreboard in main
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        guessTheNumber game = new guessTheNumber();
+        game.startDisplay();
+        game.getRange();
+        game.getNumGuesses();
+        game.getGuesses();
+
+    }
+
+}
---
+++ hideTheThimble.java
@@ -0,0 +1,191 @@
+/*
+ * header
+ */
+
+import java.util.*;
+
+public class hideTheThimble {
+
+    // declare variables
+    int bestOfNum;
+    int playerScore = 0;
+    int computerScore = 0;
+    char computerHand;
+    char playerHand;
+    final char LEFTCHAR = 'L';
+    final char RIGHTCHAR = 'R';
+    final int LEFTINT = 0;
+    final int RIGHTINT = 1;
+    final int INDEX_ZERO = 0;
+    final int ZERO = 0;
+    final int ONE = 1;
+    final int TWO = 2;
+    Scanner sc = new Scanner(System.in);
+
+    private int getRandomInt(int min, int max) {
+
+        Random r = new Random();
+        return r.nextInt((max - min) + ONE) + min;
+    }
+
+    // start display method
+    public void startDisplay() {
+
+        System.out.println(
+                "The players will specify the 'best out of number' for the game. For instance, if they choose 7, a player will win the game if they successfully guess the hand holding the thimble 4 or more times. The 'best out of number' chosen must be odd");
+        System.out.println("\n"); // formatting output
+
+    }
+
+    // getBestOfNum method:
+    public void getBestOfNum() {
+
+        System.out.println("Select a best out of number, must be odd");
+        bestOfNum = sc.nextInt();
+        System.out.println("\n"); // formatting output
+
+        // make sure its odd
+        while ((bestOfNum % TWO) == ZERO) {
+
+            System.out.println("Select an Odd number");
+            bestOfNum = sc.nextInt();
+            System.out.println("\n"); // formatting output
+
+        }
+
+        /*
+         * TEST MODE
+         * System.out.println("Best out of Number: " + bestOfNum); // formatting output
+         * System.out.println("\n"); // formatting output
+         */
+
+    }
+
+    public void putThimble() {
+
+        int generator = getRandomInt(LEFTINT, RIGHTINT);
+
+        if (generator == LEFTINT) {
+
+            computerHand = LEFTCHAR;
+
+        } else { // implies generator pulled a 1, and computer selected right hand
+
+            computerHand = RIGHTCHAR;
+
+        }
+
+        /*
+         * TEST MODE
+         * System.out.println("Computer selected: " + computerHand + "\n");
+         */
+
+        getThimbleGuess();
+    }
+
+    public void getThimbleGuess() {
+
+        System.out.println("Select Hand, L or R");
+        playerHand = sc.next().charAt(INDEX_ZERO);
+        System.out.println("\n"); // formatting output
+        System.out.println("User Guess: " + playerHand + "\n");
+
+        // determine correct input
+
+        boolean correct = false;
+        int isleft = (Character.compare(playerHand, LEFTCHAR));
+        int isright = (Character.compare(playerHand, RIGHTCHAR));
+
+        while (correct == false) {
+
+            if (isleft == ZERO) { // user input is 'L'
+
+                correct = true;
+
+            } else if (isright == ZERO) { // user input is 'R'
+
+                correct = true;
+
+            } else { // implies user input is incorrect
+
+                System.out.println("Input L or R");
+                playerHand = sc.next().charAt(INDEX_ZERO);
+                System.out.println("\n"); // formatting output
+
+                // update values
+                isleft = (Character.compare(playerHand, LEFTCHAR));
+                isright = (Character.compare(playerHand, RIGHTCHAR));
+
+            }
+
+        }
+
+        /*
+         * TEST MODE
+         * System.out.println("User Guess: " + playerHand + "\n");
+         */
+
+        displayScoreboard();
+        checkGuess();
+
+    }
+
+    public void displayScoreboard() {
+
+        System.out.println("SCOREBOARD");
+        System.out.println("Player: " + playerScore + "    Computer: " + computerScore);
+
+    }
+
+    public void checkGuess() {
+
+        int same = (Character.compare(playerHand, computerHand)); // 0 if same, -1 or 1 if not
+
+        if (same == ZERO) {
+
+            playerScore += ONE; // increment by 1
+
+            // if score now equals best of num, return to main with updated GOG Score player
+            // + 1
+            if (playerScore == bestOfNum) {
+
+                displayScoreboard();
+                // implement here
+
+            } else { // implies best of num not reached yet...
+
+                putThimble();
+
+            }
+
+        } else { // implies that the guess was incorrect
+
+            computerScore += ONE; // increment by 1
+
+            // if score now equals best of num, return to main with updated GOG Score
+            // computer + 1
+            if (computerScore == bestOfNum) {
+
+                displayScoreboard();
+                // implement here
+
+            } else { // implies best of num not reached yet...
+
+                putThimble();
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        hideTheThimble game = new hideTheThimble();
+        game.startDisplay();
+        game.getBestOfNum();
+        game.putThimble();
+
+    }
+
+}
